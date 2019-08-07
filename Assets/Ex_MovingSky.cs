f70b1097b6371d2ef3fdfb1709864a8ab3d79ea0
Little Big Monkey <info@littlebigmonkey.com>
2019-08-07 15:24:28 +0200

    Add Dev Sky

---
+++ Assets/Ex_MovingSky.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_MovingSky : MonoBehaviour
+{
+
+    public Material m_skyMaterial;
+    public float m_offset;
+    public float m_speed=2f;
+    
+    void Update()
+    {
+        Vector2 offset = m_skyMaterial.GetTextureOffset("_MainTex");
+        offset.x += Time.deltaTime* m_speed;
+        m_skyMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}

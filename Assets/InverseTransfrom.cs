f01eee0b01ff282edcb4dde54938303e91916f1e
streeeloi <streeeloi@gmail.com>
2019-08-07 07:44:05 +0200

    Save Day1

--- Assets/InverseTransfrom.cs
+++ Assets/InverseTransfrom.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReloadScene : MonoBehaviour
+public class InverseTransfrom : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {

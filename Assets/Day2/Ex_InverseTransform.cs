a4fba6a25051f73340b6e0389ef07b2c795b0c47
Little Big Monkey <info@littlebigmonkey.com>
2019-08-23 12:23:47 +0200

    Fire with Curve

--- Assets/Day2/Ex_InverseTransform.cs
+++ Assets/Day2/Ex_InverseTransform.cs
@@ -10,10 +10,12 @@ public class Ex_InverseTransform : MonoBehaviour
     public bool m_isAtRight;
     public bool m_isAtUp;
     public bool m_isAtForward;
-
+    public float m_range = 5;
     void Update()
     {
-        m_gizmo.localPosition = m_a.InverseTransformPoint(m_b.position);
+        m_gizmo.localPosition = (m_a.InverseTransformPoint(m_b.position)) * m_range;
+      //  transform.InverseTransformDirection
+
         m_isAtRight = m_gizmo.localPosition.x > 0f;
         m_isAtUp = m_gizmo.localPosition.y > 0f;
         m_isAtForward = m_gizmo.localPosition.z > 0f;
--- Assets/Ex_QuaternionMultiplyVector.cs
+++ Assets/Ex_QuaternionMultiplyVector.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Ex_QuaternionMultiplyVector : MonoBehaviour
@@ -13,7 +14,12 @@ public class Ex_QuaternionMultiplyVector : MonoBehaviour
 
     void OnValidate()
     {
+
+        //transform.Rotate(Vector3.up, Space.Self);
+
         m_rotation = Quaternion.Euler(m_euleurAngle);
+
+        //V voulu = Q Rotation voulue * V direction actuel
         m_newDirection = m_rotation * m_direction;
 
 
@@ -21,4 +27,10 @@ public class Ex_QuaternionMultiplyVector : MonoBehaviour
 
 
     }
+    public static bool IsInLayerMask(int layer, LayerMask layermask)
+    {
+        return layermask == (layermask | (1 << layer));
+    }
 }
+
+
